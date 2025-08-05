fd8c4070fee39e7a2715f4a91316898b0e229eaf
amrhweedy <amrhweedy@gmail.com>
2025-08-06 00:49:38 +0300

    first project

---
+++ Docker_Elmahdy/Docker_Elmahdy/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Docker_Elmahdy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello , this is my first docker image");
+            while (true)
+            {
+                Console.Write($"\r{DateTime.Now:yyyyy:MM:dd HH:mm:ss}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
