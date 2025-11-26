4220b175f7e66fc2b8b29fe316c22e5a22f7c56b
amrhweedy <amrhweedy@gmail.com>
2025-11-26 17:40:19 +0200

    changes

--- Docker_Elmahdy/DockerCompose/Data/ApplicationDbContext.cs
+++ Docker_Elmahdy/DockerCompose/Data/ApplicationDbContext.cs
@@ -9,4 +9,18 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Employee>().HasData(
+
+       new Employee { Id = 1, Name = "Alice Johnson", Email = "alice@example.com" },
+            new Employee { Id = 2, Name = "Bob Smith", Email = "bob@example.com" },
+            new Employee { Id = 3, Name = "Charlie Brown", Email = "charlie@example.com" }
+
+            );
+
+    }
 }
---
+++ Docker_Elmahdy/DockerCompose/Migrations/20251126142818_seedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DockerCompose.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 1, "alice@example.com", "Alice Johnson" },
+                    { 2, "bob@example.com", "Bob Smith" },
+                    { 3, "charlie@example.com", "Charlie Brown" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
